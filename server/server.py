c98f04321b742b00f99f5975d773464a944067f1
Snehanshu Mishra <snehanshumishra@Snehanshus-MacBook-Air.local>
2024-01-28 00:29:46 +0530

    Adding headlines component

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from stockFinder import StockFinder
+from stockFinder import StockFinder, APIResponseFetcher
 import pandas as pd 
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -24,17 +25,52 @@ def search_stocks():
     else:
         result = None
 
-    # Perform the search using your Python code
-    # Replace this with your actual Python code to fetch data
-    # Example: result = your_python_function(search_criteria, search_value)
+    result_json = result.to_json(orient='records')
+    if isinstance(result, pd.DataFrame):
+        result_json = result.to_json(orient='records')
+    else:
+        result_json = result
+        
+    return result_json
 
-    # result_json = result.to_json(orient='records')
+@app.route('/api/industry-list', methods=['GET'])
+def find_industries():
+    searcher = StockFinder()
+    result = searcher.fetch_all_industries()
+
+    if isinstance(result, pd.DataFrame):
+        result_json = result.to_json(orient='records')
+    else:
+        result_json = result
+        
+    return result_json
+
+@app.route('/api/sentiment-score', methods=['POST'])
+def find_sentiment_score():
+    data = request.get_json()
+    ticker = data.get('ticker')
+
+    # Assuming you have a method 'search_stocks' in your StockFinder class
+    searcher = StockFinder()
+    result = searcher.by_symbol(ticker)
+
+    result_json = result.to_json(orient='records')
     if isinstance(result, pd.DataFrame):
         result_json = result.to_json(orient='records')
     else:
         result_json = result
 
-    return jsonify({'result': result_json})
+    return result_json
+        
+
+@app.route('/api/api-topic-wise', methods=['POST'])
+def topic_wise_news():
+    topic = request.get_json().get('topic')
+    api = APIResponseFetcher()
+    result_dict = api.by_topic(topic)
+
+    result_json = json.dumps(result_dict)
+    return result_json
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
